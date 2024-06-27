1a4c9a874a8f6b3cdb16022c8bc515435215853e
juzi <zzh@youxiake.com>
2024-06-27 14:39:37 +0800

    init

--- robus/api/subabase/main.py
+++ robus/api/subabase/main.py
@@ -1,9 +1,10 @@
 import json
-
+from fastapi.responses import FileResponse
 from fastapi import FastAPI
-from config import SUPABASE
-
+# from config import SUPABASE
+from pathlib import Path
 import logging
+from robus.config import SUPABASE
 
 log = logging.getLogger(__name__)
 log.setLevel("INFO")
@@ -11,12 +12,22 @@ log.setLevel("INFO")
 app = FastAPI()
 
 
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "subabase"}
+
 @app.get("/")
-def read_root():
-    return {"Hello": "subabase"}
+async def subabase_homepage():
+    file_path = Path(__file__).parent / "index.html"
+    return FileResponse(file_path)
 
 
 @app.get("/select")
 def select():
     response = SUPABASE.table('documents').select("*").execute()
     return response
+
+
+@app.post("/ask")
+def ask(body: dict):
+    return {'aa', 'sss'}
